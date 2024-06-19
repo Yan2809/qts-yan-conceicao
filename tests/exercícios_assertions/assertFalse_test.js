107ddcb4dbf4ecf7f96f3bf707d9b52f323dd787
Yan Breno <ybarutti2809@gmail.com>
2024-06-19 08:25:59 -0300

    Update assertFalse_test.js

--- tests/exercícios_assertions/assertFalse_test.js
+++ tests/exercícios_assertions/assertFalse_test.js
@@ -6,10 +6,10 @@ import { assertFalse } from "https://deno.land/std@0.224.0/testing/asserts.ts"; 
 Deno.test("Teste de valores falsos", () => {
     //Essa função verifica se o valor comparado é true ou false, a fim de garantir que ele seja falso.
 
+    
     assertFalse(1 !== 1, "1 é diferente de 1"); 
     //No exemplo, 1 não é diferente de 1, retornando assim um valor FALSE, fazendo com que essa assertion passe.
 
     assertFalse(1 !== 2, "1 é diferente de 2"); 
     //No exemplo, 1 é diferente de 2, retornando um valor TRUE, assim fazendo com essa assertion falhe.
-
 })
